2bccfe33d9b655b4f56ef9feca93fcdf31d9a8a8
Devil29 <vishalraj48@gmail.com>
2016-09-08 13:19:06 +0530

    added a new page

---
+++ app/view3/view3_test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('myApp.view3 module', function() {
+
+  beforeEach(module('myApp.view3'));
+
+  describe('view3 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      var view3Ctrl = $controller('View3Ctrl');
+      expect(view3Ctrl).toBeDefined();
+    }));
+
+  });
+});
